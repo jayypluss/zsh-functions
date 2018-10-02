634bf5fa923f4f8313c44d063475981b078297d8
Jonathan F. K <jonathanfk1@gmail.com>
2018-10-02 00:42:31 -0300

    Few fixings and organization in code.

---
+++ mount_windows.zsh
@@ -0,0 +1,4 @@
+mount_windows() {
+	echo "Mounting Windows partition"
+    sudo mount --verbose UUID=401CCBD11CCBBFE0 ~/HDD
+}
---
+++ tr.zsh
@@ -0,0 +1,3 @@
+tr() {
+  firefox "https://translate.google.com.br/?hl=en&tab=TT&authuser=0#auto/en/$argv"
+}
