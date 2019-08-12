d29a26eed2d71b15445b0633a6b1a5c043215e75
Jonathan F. K <jonathanfk1@gmail.com>
2019-08-12 00:28:08 -0300

    New install and uninstall apks functions.

---
+++ install_all_konviva_apks.zsh
@@ -0,0 +1,11 @@
+install_all_konviva_apks() {
+	adb install konviva-bayer-release-15539-1.55.39-signed.apk 
+	adb install konviva-flytour-release-15539-1.55.39-signed.apk
+	adb install konviva-gruponc-release-15539-1.55.39-signed.apk
+	adb install konviva-inpev-release-15539-1.55.39-signed.apk
+	adb install konviva-konviva-release-15539-1.55.39-signed.apk
+	adb install konviva-sabesp-release-15539-1.55.39-signed.apk
+	adb install konviva-siagri-release-15539-1.55.39-signed.apk
+	adb install konviva-unidas-release-15539-1.55.39-signed.apk
+	adb install konviva-vw-release-15539-1.55.39-signed.apk
+}
---
+++ uninstall_all_konviva_apks.zsh
@@ -0,0 +1,10 @@
+uninstall_all_konviva_apks() {
+	adb uninstall br.com.ilog.konviva.mobile.v1.bayer
+	adb uninstall br.com.ilog.konviva.mobile.v1.flytour
+	adb uninstall br.com.ilog.konviva.mobile.v1.gruponc
+	adb uninstall br.com.ilog.konviva.mobile.v1.inpev
+	adb uninstall br.com.ilog.konviva.mobile.v1
+	adb uninstall sabesp.konviva.com.br
+	adb uninstall br.com.ilog.konviva.mobile.v1.siagri
+	adb uninstall br.com.ilog.konviva.mobile.v1.volkswagen
+}
