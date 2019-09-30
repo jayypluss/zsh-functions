604fa15d5666a26fb724f5dc5124dbcf0de533d9
Jonathan F. K <jonathanfk1@gmail.com>
2019-09-30 20:13:02 -0300

    Updating adb_script.

--- adb_scripts/adb_script.zsh
+++ adb_scripts/adb_script.zsh
@@ -1,25 +1,20 @@
-#!/bin/zsh
-
-case "$1" in
+adb_script() {
+    case "$1" in
         login)
             case "$2" in
                     emulator)
                         . $ZSH_FUNCTIONS_PATH/adb_scripts/konviva_login-nexus_5_emulator.zsh
-                        exit 0
                         ;;
                      
                     motoz2play)
                         . $ZSH_FUNCTIONS_PATH/adb_scripts/konviva_login-moto_z2_play.zsh
-                        exit 0
                         ;;
                      
                     motoe5)
                         . $ZSH_FUNCTIONS_PATH/adb_scripts/konviva_login-moto_e5.zsh
-                        exit 0
                         ;;
 
                     other)
-                        exit 0
                         ;;
                      
                     *)
@@ -34,17 +29,14 @@ case "$1" in
             case "$2" in
                     emulator)
                         echo "Not implemented yet."
-                        exit 0
                         ;;
                      
                     motoz2play)
                         . $ZSH_FUNCTIONS_PATH/adb_scripts/konviva_logout_moto_z2_play.zsh
-                        exit 0
                         ;;
                      
                     motoe5)
                         echo "Not implemented yet."
-                        exit 0
                         ;;
                      
                     *)
@@ -59,15 +51,16 @@ case "$1" in
             echo "emulator"
             echo "motoz2play"
             echo "motoe5"
-            exit 0
             ;;
 
         testif)
-            exit 0
+
             ;;
          
         *)
             echo $"Usage: $0 {login|logout|ls}"
             exit 1
  
-esac
+    esac
+
+}
